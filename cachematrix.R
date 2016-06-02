138c0962de491d41b66c5301b96ed0e8f33c3eb9
skrishnan07 <skrishnan07@gmail.com>
2016-06-02 01:07:53 -0700

    Added an example run of the function
    Added output from the R console demonstrating successful operation of
    the functions

--- cachematrix.R
+++ cachematrix.R
@@ -51,3 +51,51 @@ cacheSolve <- function(x, ...)
         x$setInverse(invMat)
         invMat
 }
+
+
+## Here is a sample run of the function to show it works
+## Initialize a 4x4 random matrix
+
+# x <- makeCacheMatrix(matrix(rnorm(16), nrow=4, ncol=4))
+
+# x$get()
+# [,1]       [,2]       [,3]         [,4]
+# [1,]  0.52993922  1.4499146  0.4004010  1.455949605
+# [2,]  1.42466179 -0.8880387  1.6081338  0.004537156
+# [3,] -0.85988947  1.7895647 -0.1548914 -0.438168382
+# [4,]  0.05809249 -0.6003635  0.2875434  0.378736494
+
+
+## Now compute the inverse
+
+# cacheSolve(x)
+# [,1]        [,2]       [,3]       [,4]
+# [1,]  0.2763462  0.20793996 -0.7811058 -1.9685070
+# [2,]  0.2202366  0.07282495  0.2160123 -0.5976031
+# [3,] -0.1243312  0.47862741  0.8117097  1.4113079
+# [4,]  0.4011206 -0.27983687 -0.1540370  0.9235027
+
+
+## Call the function again and this time assign it to y
+
+# y <- cacheSolve(x)
+# getting cached data
+# y
+# [,1]        [,2]       [,3]       [,4]
+# [1,]  0.2763462  0.20793996 -0.7811058 -1.9685070
+# [2,]  0.2202366  0.07282495  0.2160123 -0.5976031
+# [3,] -0.1243312  0.47862741  0.8117097  1.4113079
+# [4,]  0.4011206 -0.27983687 -0.1540370  0.9235027
+
+
+## Do a sanity check on the inverse computation
+## The round is needed for clarity. 
+## Otherwise the off-diagonal entries show up as very small non-zero numbers
+
+# round(x$get() %*% y)
+# [,1] [,2] [,3] [,4]
+# [1,]    1    0    0    0
+# [2,]    0    1    0    0
+# [3,]    0    0    1    0
+# [4,]    0    0    0    1
+
